6a05f9b468c0742ccc433a4d8bb52b4af968cf9b
Matias Siro <matias.siro@outlook.com>
2021-11-06 14:30:28 +0200

    haarautumakattavuus nostettu 100:n

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,46 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_negatiivinen_luku(self):
+        self.varasto.lisaa_varastoon(-1)
+        
+        
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+        
+    def test_alkusaldo_negatiivinen(self):
+        varasto2 = Varasto(10, -1)
+        #varasto2.saldo = -1
+        
+        self.assertAlmostEqual(varasto2.saldo, 0)
+
+    def test_alku_tilavuus_negatiivinen(self):
+        varasto2 = Varasto(-1)
+        
+        self.assertAlmostEqual(varasto2.tilavuus, 0.0)
+        
+    def test_maara_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(12)
+        		
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+        
+    def test_ota_varastosta_negatiivinen_maara(self):
+        return_value = self.varasto.ota_varastosta(-1)   
+        
+        self.assertAlmostEqual(return_value, 0)
+        
+    def test_maara_enemman_kuin_saldo(self):
+        alkusaldo = 10
+        self.varasto.saldo = alkusaldo
+        return_value = self.varasto.ota_varastosta(11)
+        
+        self.assertAlmostEqual(return_value, alkusaldo)
+        
+    def test_to_string(self):
+        alkusaldo = 5
+        self.varasto.saldo = alkusaldo
+        
+        to_string = "saldo = 5, vielä tilaa 5"
+        to_string2 = self.varasto.__str__()
+        
+        self.assertAlmostEqual(to_string, to_string2)
