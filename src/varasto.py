fd13a50b5051cef460ef822d6e35854e2cec31ae
Matias Siro <matias.siro@outlook.com>
2021-11-12 14:06:29 +0200

    korjattu kaikki säännöt

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,13 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
+        '''
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+        '''
+        self.set_tilavuus(tilavuus)
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -16,7 +18,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms. xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -31,35 +33,21 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
-            
-            if maara == 0:
-                if maara == 1:
-                    maara = 1
 
             return kaikki_mita_voidaan
 
         self.saldo = self.saldo - maara
-        
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        self.paljonko_mahtuu()
-        
-
         return maara
-    
+
+    def set_tilavuus(self, tilavuus):
+        if tilavuus > 0.0:
+            self.tilavuus = tilavuus
+        else:
+            # virheellinen, nollataan
+            self.tilavuus = 0.0
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
